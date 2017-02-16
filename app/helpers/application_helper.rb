4f02a910396fc23d1660541f7a9f14e2e3a06559
Andrew Kim <awkim.11@gmail.com>
2017-02-16 15:04:57 -0500

    Completed feature order details.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def format_price(price)
+    @price = price/100.00
+    "$#{number_with_delimiter(@price)}"
+  end
+
+  def calculate_tax(price)
+    @tax = price * 0.13
+    "#{format_price(@tax)}"
+  end
+
 end

7ec15a92e4b44587552651e2339e113d5d652977
Andrew Kim <awkim.11@gmail.com>
2017-02-20 14:25:03 -0500

    First commit. Completed initial validation.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,14 @@
 class Product < ActiveRecord::Base
 
-  monetize :price_cents, numericality: true
+  monetize :price_cents, numericality: true, presence: true
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
   has_many :reviews
 
   validates :name, presence: true
-  validates :price, presence: true
-  validates :quantity, presence: true
-  validates :category, presence: true
+  validates :price_cents, numericality: true, presence: true
+  validates :quantity, numericality: { only_integer: true }, presence: true
+  validates :category_id, presence: true
 
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,102 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  context "Validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:price_cents) }
+    it { is_expected.to validate_presence_of(:quantity) }
+    it { is_expected.to validate_numericality_of(:quantity) }
+    it { is_expected.to validate_presence_of(:category_id) }
+  end
+
+  context "Product that is valid" do
+    before(:each) do
+      @category = Category.create(name: "Furniture")
+      @product = Product.new({
+        name: "Racecar Bed",
+        price_cents: 696969,
+        quantity: 69,
+        category_id: @category.id
+        })
+    end
+
+    it "Should have a name" do
+      expect(@product.name).to eq("Racecar Bed")
+    end
+
+    it "Should have a valid price " do
+      expect(@product.price_cents).to be_an_instance_of(Fixnum)
+      expect(@product.price_cents).to eq(696969)
+    end
+
+    it "Should have a valid quantity that equals" do
+      expect(@product.quantity).to eq(69)
+    end
+
+    it "Should have a valid category_id" do
+      expect(@product.category_id).to be_an_instance_of(Fixnum)
+    end
+  end
+
+  context "Product without a valid name" do
+    it "Should not have a valid name" do
+      @product = Product.create({
+        price_cents: 10000,
+        quantity: 100,
+        category_id: 1
+        })
+      expect(@product.name).to eq(nil)
+    end
+  end
+
+  # Test returned 0 for price
+  context "Product without a valid price" do
+    it "Should not have a valid price" do
+      @product = Product.create({
+        name: "jet ski",
+        price_cents: "Matt Damon",
+        quantity: 100,
+        category_id: 1
+        })
+      expect(@product.errors.full_messages).to include("is not a number")
+      expect(@product.price_cents).to_not be_an_instance_of(Integer)
+      expect(@product.price_cents).to eq(0)
+    end
+  end
+
+  context "Product without a valid quantity" do
+    it "Should not have a valid quantity" do
+      @product = Product.create({
+        name: "jet ski",
+        price_cents: 54321,
+        quantity: "House",
+        category_id: 1
+        })
+      expect(@product.quantity).to_not be_an_instance_of(Integer)
+      expect(@product.quantity).to eq(0)
+    end
+  end
+
+  context "Product without a valid category_id" do
+    it "Should not have a valid category_id" do
+      @product = Product.create({
+        name: "jet ski",
+        price_cents: 54321,
+        quantity: 123
+        })
+      expect(@product.category_id).to be(nil)
+    end
+  end
+
+end
+
+
+# belongs_to :category
+# has_many :reviews
+#
+# validates :name, presence: true
+# validates :price, presence: true
+# validates :quantity, presence: true
+# validates :category, presence: true
